2ce0ab0d8ef892c0f1b5ffd9d569c304770515fb
marten.hedborg <marten.hedborg@gmail.com>
2014-09-13 22:21:04 +0200

    Fully qualified path is now used as name for projects

--- plugin/src/main/groovy/com/m8rten/gradle/prism/plugin/InvocationBuilder.groovy
+++ plugin/src/main/groovy/com/m8rten/gradle/prism/plugin/InvocationBuilder.groovy
@@ -1,17 +1,16 @@
 package com.m8rten.gradle.prism.plugin
 import com.m8rten.gradle.prism.model.Invocation
 import com.m8rten.gradle.prism.model.Project
-import org.gradle.api.Task
+import com.m8rten.gradle.prism.model.Task
 
 class InvocationBuilder extends Invocation{
 
-    void add(Task task){
-        println task.state.failure == null
-        com.m8rten.gradle.prism.model.Task taskToSave = new com.m8rten.gradle.prism.model.Task(name: task.name,
+    void add(org.gradle.api.Task task){
+       Task taskToSave = new Task(name: task.name,
                 success: task.state.failure == null,
                 failedMessage: task.state.failure != null ? task.state.failure.message : null)
-        if(hasProjectFor(task)){
-            Project project = new Project(name: task.project.name)
+        if(projectHasBeenSavedFor(task)){
+            Project project = new Project(name: pathFor(task))
             project.tasks.add(taskToSave)
             projects.add(project)
         } else {
@@ -19,11 +18,15 @@ class InvocationBuilder extends Invocation{
         }
     }
 
-    private Project projectFor(Task task) {
-        projects.find {it.name.equals(task.project.name)}
+    private Project projectFor(org.gradle.api.Task task) {
+        projects.find {it.name.equals(pathFor(task))}
     }
 
-    private boolean hasProjectFor(Task task){
-        projects.findAll{it.name.equals(task.project.name)}.size()==0
+    private boolean projectHasBeenSavedFor(org.gradle.api.Task task){
+        projects.findAll{it.name.equals(pathFor(task))}.size()==0
+    }
+
+    private String pathFor(org.gradle.api.Task task){
+        task.project.rootProject.name+task.project.path
     }
 }
